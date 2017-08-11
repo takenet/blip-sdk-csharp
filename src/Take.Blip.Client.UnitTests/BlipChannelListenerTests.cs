9cadbaee1756f52abbdd635ced5f12d463439371
André Bires <andreb@take.net>
2017-08-11 16:02:51 -0300

    Added receivers predicate and priority tests

--- src/Take.Blip.Client.UnitTests/BlipChannelListenerTests.cs
+++ src/Take.Blip.Client.UnitTests/BlipChannelListenerTests.cs
@@ -94,9 +94,82 @@ namespace Take.Blip.Client.UnitTests
             target.Start(EstablishedReceiverChannel);
 
             // Assert
-            await Task.Delay(250);
-            messageReceiver.Received(1).ReceiveAsync(Arg.Any<Message>(), Arg.Any<CancellationToken>());
+            await Task.Delay(250);            
             messageReceiver.Received(1).ReceiveAsync(textMessage, Arg.Any<CancellationToken>());
+            messageReceiver.DidNotReceive().ReceiveAsync(chatStateMessage, Arg.Any<CancellationToken>());
+        }
+
+        [Fact]
+        public async Task RegisteringMultipleMessageReceiverWithSamePredicateShouldBeCalledWhenSpecificMessageTypeIsReceived()
+        {
+            // Arrange
+            var target = GetTarget();
+            var textMessageReceiver1 = Substitute.For<IMessageReceiver>();
+            var textMessageReceiver2 = Substitute.For<IMessageReceiver>();
+            var textMessage = Dummy.CreateMessage(Dummy.CreateTextContent());            
+            await EstablishedReceiverChannel.MessageBuffer.SendAsync(textMessage);
+
+            // Act
+            target.AddMessageReceiver(textMessageReceiver1, m => m.Type.Equals(PlainText.MediaType).AsCompletedTask());
+            target.AddMessageReceiver(textMessageReceiver2, m => m.Type.Equals(PlainText.MediaType).AsCompletedTask());
+            target.Start(EstablishedReceiverChannel);
+
+            // Assert
+            await Task.Delay(250);
+            textMessageReceiver1.Received(1).ReceiveAsync(textMessage, Arg.Any<CancellationToken>());
+            textMessageReceiver2.Received(1).ReceiveAsync(textMessage, Arg.Any<CancellationToken>());
+        }
+
+        [Fact]
+        public async Task RegisteringMultipleMessageReceiverWithDifferentTypePredicateShouldBeCalledWhenSpecificMessageTypeIsReceived()
+        {
+            // Arrange
+            var target = GetTarget();
+            var textMessageReceiver = Substitute.For<IMessageReceiver>();
+            var chatStateMessageReceiver = Substitute.For<IMessageReceiver>();            
+            var textMessage = Dummy.CreateMessage(Dummy.CreateTextContent());
+            var chatStateMessage = Dummy.CreateMessage(Dummy.CreateChatState());            
+            await EstablishedReceiverChannel.MessageBuffer.SendAsync(textMessage);
+            await EstablishedReceiverChannel.MessageBuffer.SendAsync(chatStateMessage);
+
+            // Act
+            target.AddMessageReceiver(textMessageReceiver, m => m.Type.Equals(PlainText.MediaType).AsCompletedTask());
+            target.AddMessageReceiver(chatStateMessageReceiver, m => m.Type.Equals(ChatState.MediaType).AsCompletedTask());
+            target.Start(EstablishedReceiverChannel);
+
+            // Assert
+            await Task.Delay(250);
+            textMessageReceiver.Received(1).ReceiveAsync(textMessage, Arg.Any<CancellationToken>());
+            textMessageReceiver.DidNotReceive().ReceiveAsync(chatStateMessage, Arg.Any<CancellationToken>());
+            chatStateMessageReceiver.Received(1).ReceiveAsync(chatStateMessage, Arg.Any<CancellationToken>());
+            chatStateMessageReceiver.DidNotReceive().ReceiveAsync(textMessage, Arg.Any<CancellationToken>());
+        }
+
+        [Fact]
+        public async Task RegisteringMultipleMessageReceiverWithDifferentTypePrioritiesShouldBeCalledWhenSpecificMessageTypeIsReceived()
+        {
+            // Arrange
+            var target = GetTarget();
+            var textMessageReceiver1 = Substitute.For<IMessageReceiver>();
+            var textMessageReceiver2 = Substitute.For<IMessageReceiver>();
+            var textContent1 = "first expected content";
+            var textContent2 = "not expected content";
+            var textMessage1 = Dummy.CreateMessage(textContent1);
+            var textMessage2 = Dummy.CreateMessage(textContent2);
+            await EstablishedReceiverChannel.MessageBuffer.SendAsync(textMessage1);
+            await EstablishedReceiverChannel.MessageBuffer.SendAsync(textMessage2);
+
+            // Act
+            target.AddMessageReceiver(textMessageReceiver1, m => (m.Type.Equals(PlainText.MediaType) && m.Content.ToString().Equals(textContent1)).AsCompletedTask(), 1);
+            target.AddMessageReceiver(textMessageReceiver2, null, 2);
+            target.Start(EstablishedReceiverChannel);
+
+            // Assert
+            await Task.Delay(250);
+            textMessageReceiver1.Received(1).ReceiveAsync(textMessage1, Arg.Any<CancellationToken>());
+            textMessageReceiver1.DidNotReceive().ReceiveAsync(textMessage2, Arg.Any<CancellationToken>());
+            textMessageReceiver2.Received(1).ReceiveAsync(textMessage2, Arg.Any<CancellationToken>());
+            textMessageReceiver2.DidNotReceive().ReceiveAsync(textMessage1, Arg.Any<CancellationToken>());
         }
     }
 }
