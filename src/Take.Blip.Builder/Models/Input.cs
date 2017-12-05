de8f3029592ea730ea4fea9bd26d5b3f3d8c7c01
André Bires Fonseca <andrebires@gmail.com>
2017-12-05 19:40:25 -0200

    Added input variable validation

--- src/Take.Blip.Builder/Models/Input.cs
+++ src/Take.Blip.Builder/Models/Input.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 using Lime.Messaging.Contents;
 
 namespace Take.Blip.Builder.Models
@@ -8,6 +9,8 @@ namespace Take.Blip.Builder.Models
     /// </summary>
     public class Input : IValidable
     {
+        private static readonly Regex VariableValidationRegex = new Regex("^([a-zA-Z0-9\\.]+)$", RegexOptions.Compiled);
+
         /// <summary>
         /// Indicates that the state input should be skipped.
         /// </summary>
@@ -38,7 +41,13 @@ namespace Take.Blip.Builder.Models
                 {
                     throw new ValidationException("The media type should be provided when using the 'type' validation rule");
                 }
-            }            
+            }
+
+            if (!string.IsNullOrWhiteSpace(Variable) 
+                && !VariableValidationRegex.IsMatch(Variable))
+            {
+                throw new ValidationException("The input variable name should be composed only by letters, numbers and dots");
+            }
         }
     }
 }
