0a8f2bfd8211736872648bb5ce784ca1ca138262
Rafael Pacheco <ravpacheco@gmail.com>
2017-12-12 12:49:15 -0200

    Add eventTrackAction and unit tests

---
+++ src/Take.Blip.Builder.UnitTests/Actions/EventTrackActionTests.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json.Linq;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Take.Blip.Builder.Actions;
+using Take.Blip.Client.Extensions.Bucket;
+using Take.Blip.Client.Extensions.EventTracker;
+using Xunit;
+
+namespace Take.Blip.Builder.UnitTests.Actions
+{
+    public class EventTrackActionTests : IDisposable
+    {
+        public EventTrackActionTests()
+        {
+            EventTrackExtension = Substitute.For<IEventTrackExtension>();
+            Context = Substitute.For<IContext>();
+
+            CancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+        }
+
+        public CancellationToken CancellationToken => CancellationTokenSource.Token;
+
+        public CancellationTokenSource CancellationTokenSource { get; set; }
+
+        public IEventTrackExtension EventTrackExtension { get; private set; }
+        public IContext Context { get; private set; }
+
+        public void Dispose()
+        {
+            CancellationTokenSource.Dispose();
+        }
+
+        [Fact]
+        public async Task ValidEventTrackWithIdentityShouldSucced()
+        {
+            var eventTrackAction = new EventTrackAction(EventTrackExtension);
+
+            var settings = new JObject();
+            settings["category"] = "categoryX";
+            settings["action"] = "actionA";
+            settings["identity"] = "myidentity@msging.net";
+            settings["extras"] = null;
+
+            await eventTrackAction.ExecuteAsync(Context, null, CancellationToken);
+        }
+
+    }
+}
---
+++ src/Take.Blip.Builder/Actions/EventTrackAction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Lime.Protocol.Serialization.Newtonsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Take.Blip.Client;
+using Take.Blip.Client.Extensions.EventTracker;
+using Takenet.Iris.Messaging.Resources;
+
+namespace Take.Blip.Builder.Actions
+{
+    public class EventTrackAction : IAction
+    {
+        private readonly IEventTrackExtension _eventTrackExtension;
+        private readonly JsonSerializer _serializer;
+
+        private const string CATEGORY_KEY = "category";
+        private const string ACTION_KEY = "action";
+        private const string IDENTITY_KEY = "identity";
+        private const string EXTRAS_KEY = "extras";
+
+        public string Type => "EventTrack";
+
+        public EventTrackAction(IEventTrackExtension eventTrackExtension)
+        {
+            _eventTrackExtension = eventTrackExtension;
+            _serializer = JsonSerializer.Create(JsonNetSerializer.Settings);
+        }
+
+        public async Task ExecuteAsync(IContext context, JObject settings, CancellationToken cancellationToken)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            if (settings == null) throw new ArgumentNullException(nameof(settings), "The settings are required for 'SendMessage' action");
+
+            var category = (string)settings[CATEGORY_KEY];
+            var action = (string)settings[ACTION_KEY];
+            var identity = (string)settings[IDENTITY_KEY];
+            var extras = settings[EXTRAS_KEY].ToObject<Dictionary<string, string>>();
+
+            await _eventTrackExtension.AddAsync(category, action, extras, cancellationToken, identity);
+        }
+    }
+}
