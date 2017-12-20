a277a57bef8cab6810256f736e516ab05128aa84
André Bires Fonseca <andrebires@gmail.com>
2017-12-20 13:32:05 -0200

    Added unix time calendar variables

--- src/Take.Blip.Builder/CalendarVariablesProvider.cs
+++ src/Take.Blip.Builder/CalendarVariablesProvider.cs
@@ -13,24 +13,40 @@ namespace Take.Blip.Builder
             {
                 case "datetime":
                     return now.ToString("s", CultureInfo.InvariantCulture);
+
                 case "date":
                     return now.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+
                 case "day":
                     return now.Day.ToString();
+
                 case "month":
                     return now.Month.ToString();
+
                 case "year":
                     return now.Year.ToString();
+
                 case "time":
                     return now.ToString("t", CultureInfo.InvariantCulture);
+
                 case "hour":
                     return now.Hour.ToString();
+
                 case "minute":
                     return now.Minute.ToString();
+
                 case "second":
                     return now.Second.ToString();
+
+                case "unixtime":
+                    return now.ToUnixTimeSeconds().ToString();
+
+                case "unixtimemilliseconds":
+                    return now.ToUnixTimeMilliseconds().ToString();
+
                 case "dayofweek":
                     return now.DayOfWeek.ToString();
+
             }
 
             return null;
