421c85a7ca85031489abe4e9ad3115ad95dd3914
André Bires Fonseca <andrebires@gmail.com>
2018-01-16 11:21:32 -0200

    Handling not found exception on bucket variable source

--- src/Take.Blip.Builder/Variables/BucketVariableProvider.cs
+++ src/Take.Blip.Builder/Variables/BucketVariableProvider.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Lime.Protocol;
+using Lime.Protocol.Network;
 using Lime.Protocol.Serialization.Newtonsoft;
 using Newtonsoft.Json;
 using Take.Blip.Client;
@@ -21,18 +22,25 @@ namespace Take.Blip.Builder.Variables
 
         public async Task<string> GetVariableAsync(string name, Identity user, CancellationToken cancellationToken)
         {
-            // We are sending the command directly here because the BucketExtension requires us to known the type.
-            var bucketCommandResult = await _sender.ProcessCommandAsync(
-                new Command()
-                {
-                    Uri = new LimeUri($"/buckets/{Uri.EscapeDataString(name)}"),
-                    Method = CommandMethod.Get,
-                },
-                cancellationToken);
+            try
+            {
+                // We are sending the command directly here because the BucketExtension requires us to known the type.
+                var bucketCommandResult = await _sender.ProcessCommandAsync(
+                    new Command()
+                    {
+                        Uri = new LimeUri($"/buckets/{Uri.EscapeDataString(name)}"),
+                        Method = CommandMethod.Get,
+                    },
+                    cancellationToken);
 
-            if (bucketCommandResult.Status != CommandStatus.Success) return null;
-            if (!bucketCommandResult.Resource.GetMediaType().IsJson) return bucketCommandResult.Resource.ToString();
-            return JsonConvert.SerializeObject(bucketCommandResult.Resource, JsonNetSerializer.Settings);
+                if (bucketCommandResult.Status != CommandStatus.Success) return null;
+                if (!bucketCommandResult.Resource.GetMediaType().IsJson) return bucketCommandResult.Resource.ToString();
+                return JsonConvert.SerializeObject(bucketCommandResult.Resource, JsonNetSerializer.Settings);
+            }
+            catch (LimeException ex) when (ex.Reason.Code == ReasonCodes.COMMAND_RESOURCE_NOT_FOUND)
+            {
+                return null;
+            }
         }
     }
 }
