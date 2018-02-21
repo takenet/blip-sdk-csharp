af489354666f7fe2005d3c5242fc21b73d044bcb
André Bires Fonseca <andrebires@gmal.com>
2018-02-21 09:48:32 -0300

    Added types for send raw message action

---
+++ src/Take.Blip.Builder/Actions/SendMessageFromRaw/SendMessageFromRawAction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Lime.Protocol.Serialization;
+using Newtonsoft.Json.Linq;
+using Take.Blip.Builder.Actions.ProcessHttp;
+using Take.Blip.Builder.Models;
+using Take.Blip.Client;
+
+namespace Take.Blip.Builder.Actions.SendMessageFromRaw
+{
+    public class SendMessageFromRawAction : IAction
+    {
+        private readonly ISender _sender;
+        private readonly IDocumentSerializer _documentSerializer;
+
+        public SendMessageFromRawAction(ISender sender, IDocumentSerializer documentSerializer)
+        {
+            _sender = sender;
+            _documentSerializer = documentSerializer;
+        }
+
+        public string Type => nameof(SendMessageFromRaw);
+
+        public Task ExecuteAsync(IContext context, JObject settings, CancellationToken cancellationToken)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            if (settings == null) throw new ArgumentNullException(nameof(settings));
+
+            var sendMessageFromRawSettings = settings.ToObject<SendMessageFromRawSettings>();
+
+            throw new NotImplementedException();
+        }
+    }
+
+   
+}
---
+++ src/Take.Blip.Builder/Actions/SendMessageFromRaw/SendMessageFromRawSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Take.Blip.Builder.Actions.SendMessageFromRaw
+{
+    public class SendMessageFromRawSettings
+    {
+        public string Type { get; set; }
+
+        public string RawContent { get; set; }
+
+        public Dictionary<string, string> Metadata { get; set; }
+    }
+}
