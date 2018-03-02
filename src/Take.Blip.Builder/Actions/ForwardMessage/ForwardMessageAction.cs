1f7647d5ada5874a24cfc13b3a76e82472252a2c
André Bires Fonseca <andrebires@gmal.com>
2018-03-02 18:10:56 -0300

    Added forward message action structure

---
+++ src/Take.Blip.Builder/Actions/ForwardMessage/ForwardMessageAction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Take.Blip.Builder.Actions.ForwardMessage
+{
+    public class ForwardMessageAction : ActionBase<ForwardToAgentSettings>
+    {
+        public ForwardMessageAction() 
+            : base(nameof(ForwardMessage))
+        {
+        }
+
+        public override Task ExecuteAsync(IContext context, ForwardToAgentSettings settings, CancellationToken cancellationToken)
+        {
+            
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Take.Blip.Builder/Actions/ForwardMessage/ForwardMessageSettings.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using Take.Blip.Builder.Models;
+
+namespace Take.Blip.Builder.Actions.ForwardMessage
+{
+    public class ForwardToAgentSettings : IValidable
+    {
+        public string Domain { get; set; }
+
+        public ForwardToAgentSettingsContext Context { get; set; }
+
+        public void Validate()
+        {
+            
+        }
+    }
+
+    public class ForwardToAgentSettingsContext
+    {
+        public string Type { get; set; }
+
+        public JToken Content { get; set; }
+    }
+}
