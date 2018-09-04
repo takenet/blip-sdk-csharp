de358522ec8fa1f6192759265ce1d67bc45f0675
André Bires Fonseca <andrebires@gmal.com>
2018-09-04 17:44:17 -0300

    Added support for fire and forget tracks on builder

--- src/Take.Blip.Builder/Actions/TrackEvent/TrackEventAction.cs
+++ src/Take.Blip.Builder/Actions/TrackEvent/TrackEventAction.cs
@@ -1,65 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Lime.Protocol;
-using Newtonsoft.Json.Linq;
 using Take.Blip.Client;
 using Take.Blip.Client.Extensions.EventTracker;
 
 namespace Take.Blip.Builder.Actions.TrackEvent
 {
-    public class TrackEventAction : IAction
+    public class TrackEventAction : ActionBase<TrackEventSettings>
     {
         private readonly IEventTrackExtension _eventTrackExtension;
 
-        private const string CATEGORY_KEY = "category";
-        private const string ACTION_KEY = "action";
-        private const string LABEL_KEY = "label";
-        private const string VALUE_KEY = "value";
-
-        public string Type => nameof(TrackEvent);
-
         public TrackEventAction(IEventTrackExtension eventTrackExtension)
+            : base(nameof(TrackEvent))
         {
             _eventTrackExtension = eventTrackExtension;
         }
 
-        public async Task ExecuteAsync(IContext context, JObject settings, CancellationToken cancellationToken)
+        public override Task ExecuteAsync(IContext context, TrackEventSettings settings, CancellationToken cancellationToken)
         {
-            if (context == null) throw new ArgumentNullException(nameof(context));
-            if (settings == null) throw new ArgumentNullException(nameof(settings), $"The settings are required for '{nameof(TrackEventAction)}' action");
-
-            var category = (string)settings[CATEGORY_KEY];
-            var action = (string)settings[ACTION_KEY];
-
-            if (string.IsNullOrEmpty(category)) throw new ArgumentException($"The '{nameof(category)}' settings value is required for '{nameof(TrackEventAction)}' action");
-            if (string.IsNullOrEmpty(action)) throw new ArgumentException($"The '{nameof(action)}' settings value is required for '{nameof(TrackEventAction)}' action");
-
-            var messageId = EnvelopeReceiverContext<Message>.Envelope?.Id;
-
-            Dictionary<string, string> extras;
-            if (settings.TryGetValue(nameof(extras), out var extrasToken))
-            {
-                extras = extrasToken.ToObject<Dictionary<string, string>>();
-            }
-            else
-            {
-                extras = new Dictionary<string, string>();
-            }
-
-            var valueString = (string)settings[VALUE_KEY];
-            decimal? value = null;
-            if (!string.IsNullOrEmpty(valueString))
-            {
-                decimal parsedValue;
-                if(decimal.TryParse(valueString, out parsedValue))
-                {
-                    value = parsedValue;
-                }
-            }
-
-            await _eventTrackExtension.AddAsync(category, action, label:(string)settings[LABEL_KEY], value: value, messageId: messageId, extras: extras, cancellationToken: cancellationToken, contactIdentity: context.User);
+            return _eventTrackExtension.AddAsync(
+                settings.Category,
+                settings.Action,
+                settings.Label,
+                value: settings.ParsedValue,
+                messageId: EnvelopeReceiverContext<Message>.Envelope?.Id,
+                extras: settings.Extras,
+                contactIdentity: context.User,
+                fireAndForget: settings.FireAndForget ?? true,
+                cancellationToken: cancellationToken
+                );
         }
     }
 }
---
+++ src/Take.Blip.Builder/Actions/TrackEvent/TrackEventSettings.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Take.Blip.Builder.Models;
+
+namespace Take.Blip.Builder.Actions.TrackEvent
+{
+    public class TrackEventSettings : IValidable
+    {
+        public string Category { get; set; }
+
+        public string Action { get; set; }
+
+        public string Label { get; set; }
+
+        public string Value { get; set; }
+
+        public decimal? ParsedValue
+        {
+            get
+            {
+                if (decimal.TryParse(Value, out var parsedValue))
+                {
+                    return parsedValue;
+                }
+
+                return null;
+            }
+        }
+
+        public IDictionary<string, string> Extras { get; set; }
+
+        public bool? FireAndForget { get; set; }
+
+        public void Validate()
+        {
+            if (string.IsNullOrEmpty(Category))
+            {
+                throw new ArgumentException($"The '{nameof(Category)}' settings value is required for '{nameof(TrackEventAction)}' action");
+            }
+
+            if (string.IsNullOrEmpty(Action))
+            {
+                throw new ArgumentException($"The '{nameof(Action)}' settings value is required for '{nameof(TrackEventAction)}' action");
+            }
+        }
+    }
+}
