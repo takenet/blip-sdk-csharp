3b550767067014ac379caac7935442e0ee9b1d2d
André Bires Fonseca <andrebires@gmal.com>
2018-09-05 16:28:57 -0300

    Using correct CRM and Analytics node addresses

--- src/Take.Blip.Client/Extensions/EventTracker/EventTrackExtension.cs
+++ src/Take.Blip.Client/Extensions/EventTracker/EventTrackExtension.cs
@@ -11,9 +11,10 @@ namespace Take.Blip.Client.Extensions.EventTracker
 {
     public class EventTrackExtension : ExtensionBase, IEventTrackExtension
     {
-        const string EVENTRACK_URI = "/event-track";
-        const string EVENTRACK_MESSAGE_URI = "/event-track/message";
-        readonly Node EventTrackPostmaster = Node.Parse("postmaster@analytics.msging.net");
+        private const string EVENTRACK_URI = "/event-track";
+        private const string EVENTRACK_MESSAGE_URI = "/event-track/message";
+        
+        private readonly Node AnalyticsAddress = Node.Parse("postmaster@analytics.msging.net");
 
         public EventTrackExtension(ISender sender)
             : base(sender)
@@ -82,6 +83,7 @@ namespace Take.Blip.Client.Extensions.EventTracker
             var requestCommand = new Command(null)
             {
                 Uri = new LimeUri(EVENTRACK_URI),
+                To = AnalyticsAddress,
                 Resource = new EventTrack
                 {
                     Category = category,
@@ -114,19 +116,25 @@ namespace Take.Blip.Client.Extensions.EventTracker
 
         public Task<DocumentCollection> GetAllAsync(DateTimeOffset startDate, DateTimeOffset endDate, string category, string action, int skip = 0, int take = 20, CancellationToken cancellationToken = default(CancellationToken))
         {
-            var commandRequest = CreateGetCommandRequest($"{EVENTRACK_URI}/{category}/{action}?{nameof(startDate)}={Uri.EscapeDataString(startDate.ToString("s"))}&{nameof(endDate)}={Uri.EscapeDataString(endDate.ToString("s"))}&${nameof(skip)}={skip}&{nameof(take)}={take}");
+            var commandRequest = CreateGetCommandRequest(
+                $"{EVENTRACK_URI}/{category}/{action}?{nameof(startDate)}={Uri.EscapeDataString(startDate.ToString("s"))}&{nameof(endDate)}={Uri.EscapeDataString(endDate.ToString("s"))}&${nameof(skip)}={skip}&{nameof(take)}={take}",
+                AnalyticsAddress);
             return ProcessCommandAsync<DocumentCollection>(commandRequest, cancellationToken);
         }
 
         public Task<DocumentCollection> GetCategoriesAsync(int take = 20, CancellationToken cancellationToken = default(CancellationToken))
         {
-            var commandRequest = CreateGetCommandRequest($"{EVENTRACK_URI}?$take={take}");
+            var commandRequest = CreateGetCommandRequest(
+                $"{EVENTRACK_URI}?$take={take}",
+                AnalyticsAddress);
             return ProcessCommandAsync<DocumentCollection>(commandRequest, cancellationToken);
         }
 
         public Task<DocumentCollection> GetCategoryActionsCounterAsync(DateTimeOffset startDate, DateTimeOffset endDate, string category, int take = 20, CancellationToken cancellationToken = default(CancellationToken))
         {
-            var commandRequest = CreateGetCommandRequest($"{EVENTRACK_URI}/{category}?{nameof(startDate)}={Uri.EscapeDataString(startDate.ToString("s"))}&{nameof(endDate)}={Uri.EscapeDataString(endDate.ToString("s"))}&$take={take}");
+            var commandRequest = CreateGetCommandRequest(
+                $"{EVENTRACK_URI}/{category}?{nameof(startDate)}={Uri.EscapeDataString(startDate.ToString("s"))}&{nameof(endDate)}={Uri.EscapeDataString(endDate.ToString("s"))}&$take={take}",
+                AnalyticsAddress);
             return ProcessCommandAsync<DocumentCollection>(commandRequest, cancellationToken);
         }
     }
