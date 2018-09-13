1fd32660400d1fbbf0bdd75580fff61131786252
André Bires Fonseca <andrebires@gmal.com>
2018-09-13 16:11:05 -0300

    Added trace mode extension

--- src/Take.Blip.Builder/Diagnostics/TraceMode.cs
+++ src/Take.Blip.Builder/Diagnostics/TraceMode.cs
@@ -8,4 +8,21 @@
         ErrorSlow,
         All
     }
+
+    public static class TraceModeExtensions
+    {
+        public static bool IsError(this TraceMode traceMode)
+        {
+            return
+                traceMode == TraceMode.Error ||
+                traceMode == TraceMode.ErrorSlow;
+        }
+
+        public static bool IsSlow(this TraceMode traceMode)
+        {
+            return
+                traceMode == TraceMode.Slow ||
+                traceMode == TraceMode.ErrorSlow;
+        }
+    }
 }
