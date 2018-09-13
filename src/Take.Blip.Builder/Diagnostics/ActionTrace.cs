d5b651d90223a190de20548887773dcb13a204f0
André Bires Fonseca <andrebires@gmal.com>
2018-09-13 14:35:07 -0300

    Passed trace timestamp to base class

--- src/Take.Blip.Builder/Diagnostics/ActionTrace.cs
+++ src/Take.Blip.Builder/Diagnostics/ActionTrace.cs
@@ -5,16 +5,9 @@ namespace Take.Blip.Builder.Diagnostics
 {
     public class ActionTrace : Trace
     {
-        public ActionTrace()
-        {
-            Timestamp = DateTimeOffset.UtcNow;
-        }
-
         public int Order { get; set; }
 
-        public string Type { get; set; }
-
-        public DateTimeOffset Timestamp { get; set; }
+        public string Type { get; set; }        
 
         public JObject ParsedSettings { get; set; }
     }
--- src/Take.Blip.Builder/Diagnostics/Trace.cs
+++ src/Take.Blip.Builder/Diagnostics/Trace.cs
@@ -1,7 +1,16 @@
-﻿namespace Take.Blip.Builder.Diagnostics
+﻿using System;
+
+namespace Take.Blip.Builder.Diagnostics
 {
     public class Trace
     {
+        public Trace()
+        {
+            Timestamp = DateTimeOffset.UtcNow;
+        }
+
+        public DateTimeOffset Timestamp { get; set; }
+
         public long ElapsedMilliseconds { get; set; }
 
         public string Error { get; set; }
