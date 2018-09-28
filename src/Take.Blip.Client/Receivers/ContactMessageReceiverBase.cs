4159eda38dcb2bc18905ef35ba21ed2a521431e5
André Bires <andrebires@gmail.com>
2018-09-28 10:01:40 -0300

    Updated Elephant, Redis and Lime packages

--- src/Take.Blip.Client/Receivers/ContactMessageReceiverBase.cs
+++ src/Take.Blip.Client/Receivers/ContactMessageReceiverBase.cs
@@ -103,7 +103,8 @@ namespace Take.Blip.Client.Receivers
                 contact.Gender = account.Gender;
                 contact.PhoneNumber = account.PhoneNumber;
                 contact.PhotoUri = account.PhotoUri;
-                contact.Timezone = account.Timezone;
+                contact.Offset = account.Offset;
+                contact.TimeZoneName = account.TimeZoneName;
             }
 
             return contact;
