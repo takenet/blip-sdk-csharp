55fae7a114e3c8a8a9da0b673a3080a5e3382a86
André Bires <andrebires@gmail.com>
2018-10-03 14:48:08 -0300

    Using lazy initialization on ContextProvider references

--- src/Take.Blip.Builder/ContextProvider.cs
+++ src/Take.Blip.Builder/ContextProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Lime.Protocol;
 using Take.Blip.Builder.Hosting;
 using Take.Blip.Builder.Models;
@@ -14,20 +15,18 @@ namespace Take.Blip.Builder
     public class ContextProvider : IContextProvider
     {
         private readonly IConfiguration _configuration;
-        private readonly IEnumerable<IVariableProvider> _variableProviders;
-        private readonly IContextExtension _contextExtension;
-        private readonly IOwnerCallerNameDocumentMap _ownerCallerNameDocumentMap;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly Lazy<IEnumerable<IVariableProvider>> _variableProviders;
+        private readonly Lazy<IContextExtension> _contextExtension;
+        private readonly Lazy<IOwnerCallerNameDocumentMap> _ownerCallerNameDocumentMap;
 
-        public ContextProvider(
-            IConfiguration configuration,
-            IEnumerable<IVariableProvider> variableProviders,
-            IContextExtension contextExtension,
-            IOwnerCallerNameDocumentMap ownerCallerNameDocumentMap)
+        public ContextProvider(IConfiguration configuration, IServiceProvider serviceProvider)
         {
-            _contextExtension = contextExtension;
             _configuration = configuration;
-            _variableProviders = variableProviders;
-            _ownerCallerNameDocumentMap = ownerCallerNameDocumentMap;
+            _contextExtension = new Lazy<IContextExtension>(() => _serviceProvider.GetService<IContextExtension>());
+            _variableProviders = new Lazy<IEnumerable<IVariableProvider>>(() => _serviceProvider.GetService<IEnumerable<IVariableProvider>>());
+            _ownerCallerNameDocumentMap = new Lazy<IOwnerCallerNameDocumentMap>(() => _serviceProvider.GetService<IOwnerCallerNameDocumentMap>());
+            _serviceProvider = serviceProvider;
         }
 
         public IContext CreateContext(Identity user, Identity application, LazyInput input, Flow flow)
@@ -35,11 +34,19 @@ namespace Take.Blip.Builder
             switch (_configuration.ContextType)
             {
                 case nameof(StorageContext):
-                    return new StorageContext(user, application, input, flow, _variableProviders, _ownerCallerNameDocumentMap);
+                    return new StorageContext(user, application, input, flow, _variableProviders.Value, _ownerCallerNameDocumentMap.Value);
 
                 default:
-                    return new ExtensionContext(user, application, input, flow, _variableProviders, _contextExtension);
+                    return new ExtensionContext(user, application, input, flow, _variableProviders.Value, _contextExtension.Value);
             }
+        }
+    }
+
+    public static class ServiceProviderExtensions
+    {
+        public static TService GetService<TService>(this IServiceProvider serviceProvider)
+        {
+            return (TService)serviceProvider.GetService(typeof(TService));
         }
     }
 }
