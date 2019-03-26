41d2174697ffdf9371e73f1e0191dfa6242e3079
Luiz Guilherme <luizg@takenet.com.br>
2019-03-26 12:49:53 -0300

    Add contact cache to ContactVariableProvider

---
+++ src/Take.Blip.Builder/Utils/ContactMomoryCache.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Lime.Messaging.Resources;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Take.Blip.Builder.Utils
+{
+    public class ContactMomoryCache : ICache<Contact>
+    {
+        private readonly MemoryCache _contactCache;
+        private readonly TimeSpan _cacheExpiration;
+
+        public ContactMomoryCache(
+            TimeSpan cacheExpiration = default(TimeSpan)
+            )
+        {
+            _contactCache = new MemoryCache(new MemoryCacheOptions());
+            _cacheExpiration = cacheExpiration == default(TimeSpan) ? TimeSpan.FromMinutes(30) : cacheExpiration;
+        }
+
+        public Contact Get(object key) => _contactCache.Get(key) as Contact;
+
+        public void Remove(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(object key, Contact value)
+        {
+            _contactCache.Set(key, value, DateTimeOffset.UtcNow.Add(_cacheExpiration));
+        }
+    }
+}
---
+++ src/Take.Blip.Builder/Utils/ICache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Take.Blip.Builder.Utils
+{
+    public interface ICache<TItem>
+    {
+        TItem Get(object key);
+
+        void Set(object key, TItem value);
+
+        void Remove(object key);
+    }
+}
