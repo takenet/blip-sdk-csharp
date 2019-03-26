e6cc9ed0b7b6ebc19a924d5c34322c9f3b87e75c
Luiz Guilherme <luizg@takenet.com.br>
2019-03-26 18:37:45 -0300

    Add register decorator to ServiceProvider and fix tests

--- src/Samples/Builder.Console/BuilderServiceProvider.cs
+++ src/Samples/Builder.Console/BuilderServiceProvider.cs
@@ -17,5 +17,7 @@ namespace Builder.Console
         public void RegisterService(Type serviceType, object instance) => RegisterSingleton(serviceType, instance);
 
         public void RegisterService(Type serviceType, Func<object> instanceFactory) => RegisterSingleton(serviceType, instanceFactory);
+
+        public void RegisterDecorator(Type serviceType, Func<object> instanceFactory) => RegisterDecorator(serviceType, instanceFactory);
     }
 }
