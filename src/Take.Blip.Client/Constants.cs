4aafd6787dbf4e509686db8a9793d4ae8fee916a
Lucas Mundim <lucasm@take.net>
2019-05-09 08:22:47 -0300

    create Constants with Bot Identity Header and User Identity headers to enable users to access them without possible mistakes

--- src/Take.Blip.Client/Constants.cs
+++ src/Take.Blip.Client/Constants.cs
@@ -17,5 +17,15 @@ namespace Take.Blip.Client
         public const string POSTMASTER = "postmaster";
 
         public const string DEFAULT_STATE = "default";
+
+        /// <summary>
+        /// Default header containing the user's Identity
+        /// </summary>
+        public const string BLIP_USER_HEADER = "X-Blip-User";
+
+        /// <summary>
+        /// Default header containing the bot's Identity
+        /// </summary>
+        public const string BLIP_BOT_HEADER = "X-Blip-Bot";
     }
 }
