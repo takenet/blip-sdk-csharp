29af897bd04570b360f14e66415b22384e7b6e58
André Bires <andrebires@gmail.com>
2019-05-30 13:42:51 -0300

    Moved conditions to the Condition class

---
+++ src/Take.Blip.Builder/Models/ConditionsExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Take.Blip.Builder.Models
+{
+    public static class ConditionsExtensions
+    {
+        public static async Task<bool> EvaluateConditionsAsync(
+           this IEnumerable<Condition> conditions,
+           LazyInput lazyInput,
+           IContext context,
+           CancellationToken cancellationToken)
+        {
+            foreach (var outputCondition in conditions)
+            {
+                if (!await outputCondition.EvaluateConditionAsync(lazyInput, context, cancellationToken))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
