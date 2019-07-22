546d59a1db512783a1788ac00fe6dc116cc7b4a3
Felipe Nader <felipen@take.net>
2019-07-22 18:41:15 -0300

    Changed DocumentTypeResolver to resolve new packages

--- src/Take.Blip.Client/Extensions/DocumentTypeResolverExtensions.cs
+++ src/Take.Blip.Client/Extensions/DocumentTypeResolverExtensions.cs
@@ -1,5 +1,10 @@
 ﻿using Lime.Messaging;
 using Lime.Protocol.Serialization;
+using Takenet.Iris.Messaging.Contents;
+using Takenet.Iris.Messaging.Resources.ArtificialIntelligence;
+using Takenet.Iris.Messaging.Resources.Desk;
+using Takenet.Iris.Messaging.Resources.Media;
+using Takenet.Iris.Messaging.Resources.Portal;
 
 namespace Take.Blip.Client.Extensions
 {
@@ -8,7 +13,11 @@ namespace Take.Blip.Client.Extensions
         public static IDocumentTypeResolver WithBlipDocuments(this IDocumentTypeResolver documentTypeResolver)
         {
             documentTypeResolver = documentTypeResolver.WithMessagingDocuments();
-            documentTypeResolver.RegisterAssemblyDocuments(typeof(Takenet.Iris.Messaging.Contents.Attendance).Assembly);
+            documentTypeResolver.RegisterAssemblyDocuments(typeof(Attendance).Assembly);
+            documentTypeResolver.RegisterAssemblyDocuments(typeof(Intention).Assembly);
+            documentTypeResolver.RegisterAssemblyDocuments(typeof(Attendant).Assembly);
+            documentTypeResolver.RegisterAssemblyDocuments(typeof(DetailMedia).Assembly);
+            documentTypeResolver.RegisterAssemblyDocuments(typeof(Application).Assembly);
             documentTypeResolver.RegisterAssemblyDocuments(typeof(DocumentTypeResolverExtensions).Assembly);
             return documentTypeResolver;
         }
