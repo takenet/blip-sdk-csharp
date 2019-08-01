0103bbdfe3b00a7c9c220ee7aaef70311f9b970f
Arthur Vinicius Soares Pereira <arthurs@take.net>
2019-08-01 09:48:27 -0300

    Pull requests ajustments

--- src/Take.Blip.Builder.UnitTests/LazyInputTests.cs
+++ src/Take.Blip.Builder.UnitTests/LazyInputTests.cs
@@ -11,6 +11,7 @@ using Xunit;
 using Input = Take.Blip.Builder.Models.Input;
 using Newtonsoft.Json;
 using Lime.Protocol.Serialization.Newtonsoft;
+using System.Threading;
 
 #pragma warning disable 4014
 
@@ -20,20 +21,20 @@ namespace Take.Blip.Builder.UnitTests
 
     {
         [Fact]
-        public async Task ArtificialIntelligenceExtensionAnalyzeAsyncShouldBeExecuted()
+        public async Task MessageWithAnalyzableMetadataShouldExecuteArtificialIntelligenceAnalyse()
         {
             // Arrange
             var requestJson = "{\"dialogState\":\"STARTED\",\"intent\":{\"confirmationStatus\":\"NONE\",\"name\":\"PlanMyTrip\",\"slots\":{\"fromCity\":{\"confirmationStatus\":\"NONE\",\"name\":\"fromCity\"},\"SlotName\":{\"confirmationStatus\":\"NONE\",\"name\":\"string\",\"resolutions\":{\"resolutionsPerAuthority\":[{\"authority\":\"string\",\"status\":{\"code\":\"ER_SUCCESS_MATCH\"},\"values\":[{\"value\":{\"name\":\"string\",\"id\":\"string\"}}]}]},\"value\":\"string\"},\"toCity\":{\"confirmationStatus\":\"NONE\",\"name\":\"toCity\",\"value\":\"Chicago\"},\"travelDate\":{\"confirmationStatus\":\"NONE\",\"name\":\"travelDate\"}}},\"locale\":\"en-US\",\"reason\":\"USER_INITIATED\",\"requestId\":\"amzn1.echo-api.request.1\",\"timestamp\":\"2019-03-23T00:34:14.000Z\",\"type\":\"IntentRequest\"}";
             var json = JsonConvert.DeserializeObject<Dictionary<string, object>>(requestJson);
 
-            var MessageWithAnalyzable = new Message()
+            var messageWithAnalyzable = new Message()
             {
                 Content = new JsonDocument(json, MediaType.ApplicationJson),
                 Metadata = new Dictionary<string, string> { { "ai.analyzable", "true" }, { "key", "value" } }
             };
             
-            Input = new LazyInput(
-               MessageWithAnalyzable,
+            var jsonInput = new LazyInput(
+               messageWithAnalyzable,
                new BuilderConfiguration(),
                new DocumentSerializer(new DocumentTypeResolver()),
                new EnvelopeSerializer(new DocumentTypeResolver()),
@@ -41,30 +42,30 @@ namespace Take.Blip.Builder.UnitTests
                CancellationToken);
 
             // Act
-            await Input.AnalyzedContent;
+            await jsonInput.AnalyzedContent;
 
             // Assert
-            Input.Analyzable.ShouldBe(true);
+            jsonInput.Analyzable.ShouldBe(true);
             await ArtificialIntelligenceExtension.Received(1).AnalyzeAsync(Arg.Is<AnalysisRequest>(
                 a => a.Text.Equals(requestJson)
             ), CancellationToken);
         }
 
         [Fact]
-        public async Task ArtificialIntelligenceExtensionAnalyzeAsyncShouldNotBeExecuted()
+        public async Task MessageWithoutAnalyzableMetadataShouldNotExecuteArtificialIntelligenceAnalyse()
         {
             // Arrange
             var requestJson = "{\"dialogState\":\"STARTED\",\"intent\":{\"confirmationStatus\":\"NONE\",\"name\":\"PlanMyTrip\",\"slots\":{\"fromCity\":{\"confirmationStatus\":\"NONE\",\"name\":\"fromCity\"},\"SlotName\":{\"confirmationStatus\":\"NONE\",\"name\":\"string\",\"resolutions\":{\"resolutionsPerAuthority\":[{\"authority\":\"string\",\"status\":{\"code\":\"ER_SUCCESS_MATCH\"},\"values\":[{\"value\":{\"name\":\"string\",\"id\":\"string\"}}]}]},\"value\":\"string\"},\"toCity\":{\"confirmationStatus\":\"NONE\",\"name\":\"toCity\",\"value\":\"Chicago\"},\"travelDate\":{\"confirmationStatus\":\"NONE\",\"name\":\"travelDate\"}}},\"locale\":\"en-US\",\"reason\":\"USER_INITIATED\",\"requestId\":\"amzn1.echo-api.request.1\",\"timestamp\":\"2019-03-23T00:34:14.000Z\",\"type\":\"IntentRequest\"}";
             var json = JsonConvert.DeserializeObject<Dictionary<string, object>>(requestJson);
 
-            var MessageWithAnalyzable = new Message()
+            var messageWithAnalyzable = new Message()
             {
                 Content = new JsonDocument(json, MediaType.ApplicationJson),
                 Metadata = new Dictionary<string, string> { { "key", "value" } }
             };
 
-            Input = new LazyInput(
-               MessageWithAnalyzable,
+            var jsonInput = new LazyInput(
+               messageWithAnalyzable,
                new BuilderConfiguration(),
                new DocumentSerializer(new DocumentTypeResolver()),
                new EnvelopeSerializer(new DocumentTypeResolver()),
@@ -72,29 +73,29 @@ namespace Take.Blip.Builder.UnitTests
                CancellationToken);
 
             // Act
-            await Input.AnalyzedContent;
+            await jsonInput.AnalyzedContent;
 
             // Assert
-            Input.Analyzable.ShouldBe(false);
-            await ArtificialIntelligenceExtension.Received(0).AnalyzeAsync(Arg.Is<AnalysisRequest>(
-                a => a.Text.Equals(requestJson)
-            ), CancellationToken);
+            jsonInput.Analyzable.ShouldBe(false);
+            await ArtificialIntelligenceExtension.Received(0).AnalyzeAsync(
+                Arg.Any<AnalysisRequest>(), Arg.Any<CancellationToken>()
+            );
         }
 
         [Fact]
-        public async Task ArtificialIntelligenceExtensionAnalyzeAsyncShouldNotBeExecutedWithoutMetadata()
+        public async Task MessageWithoutMetadataShouldNotExecuteArtificialIntelligenceAnalyse()
         {
             // Arrange
             var requestJson = "{\"dialogState\":\"STARTED\",\"intent\":{\"confirmationStatus\":\"NONE\",\"name\":\"PlanMyTrip\",\"slots\":{\"fromCity\":{\"confirmationStatus\":\"NONE\",\"name\":\"fromCity\"},\"SlotName\":{\"confirmationStatus\":\"NONE\",\"name\":\"string\",\"resolutions\":{\"resolutionsPerAuthority\":[{\"authority\":\"string\",\"status\":{\"code\":\"ER_SUCCESS_MATCH\"},\"values\":[{\"value\":{\"name\":\"string\",\"id\":\"string\"}}]}]},\"value\":\"string\"},\"toCity\":{\"confirmationStatus\":\"NONE\",\"name\":\"toCity\",\"value\":\"Chicago\"},\"travelDate\":{\"confirmationStatus\":\"NONE\",\"name\":\"travelDate\"}}},\"locale\":\"en-US\",\"reason\":\"USER_INITIATED\",\"requestId\":\"amzn1.echo-api.request.1\",\"timestamp\":\"2019-03-23T00:34:14.000Z\",\"type\":\"IntentRequest\"}";
             var json = JsonConvert.DeserializeObject<Dictionary<string, object>>(requestJson);
 
-            var MessageWithAnalyzable = new Message()
+            var messageWithAnalyzable = new Message()
             {
                 Content = new JsonDocument(json, MediaType.ApplicationJson)
             };
 
-            Input = new LazyInput(
-               MessageWithAnalyzable,
+            var jsonInput = new LazyInput(
+               messageWithAnalyzable,
                new BuilderConfiguration(),
                new DocumentSerializer(new DocumentTypeResolver()),
                new EnvelopeSerializer(new DocumentTypeResolver()),
@@ -102,13 +103,13 @@ namespace Take.Blip.Builder.UnitTests
                CancellationToken);
 
             // Act
-            await Input.AnalyzedContent;
+            await jsonInput.AnalyzedContent;
 
             // Assert
-            Input.Analyzable.ShouldBe(false);
-            await ArtificialIntelligenceExtension.Received(0).AnalyzeAsync(Arg.Is<AnalysisRequest>(
-                a => a.Text.Equals(requestJson)
-            ), CancellationToken);
+            jsonInput.Analyzable.ShouldBe(false);
+            await ArtificialIntelligenceExtension.Received(0).AnalyzeAsync(
+                Arg.Any<AnalysisRequest>(), Arg.Any<CancellationToken>()
+            );
         }
     }
 }
