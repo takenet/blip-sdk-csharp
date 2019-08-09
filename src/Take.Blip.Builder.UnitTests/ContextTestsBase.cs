38b33e1833478364685be44dcd22b07558f75f82
André Bires <andrebires@gmail.com>
2019-08-09 16:41:44 -0300

    Added TicketVariableProvider tests

---
+++ src/Take.Blip.Builder.UnitTests/ContextTestsBase.cs
@@ -0,0 +1,29 @@
+using System;
+using Lime.Protocol;
+using NSubstitute;
+using Take.Blip.Builder.Models;
+
+namespace Take.Blip.Builder.UnitTests
+{
+    public class ContextTestsBase : CancellationTokenTestsBase
+    {
+        public ContextTestsBase()
+        {
+            Context = Substitute.For<IContext>();
+            Flow = new Flow();
+            UserIdentity = new Identity(Guid.NewGuid().ToString(), "msging.net");
+            OwnerIdentity = new Identity("application", "msging.net");
+            Context.Flow.Returns(Flow);
+            Context.UserIdentity.Returns(UserIdentity);
+            Context.OwnerIdentity.Returns(OwnerIdentity);
+        }
+        
+        public IContext Context { get; }
+        
+        public Flow Flow { get; }
+        
+        public Identity UserIdentity { get; }
+
+        public Identity OwnerIdentity { get; }
+    }
+}
