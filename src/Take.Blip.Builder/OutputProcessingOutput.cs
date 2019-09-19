fc81dcdbe4f9d70b2127375a4d9c7e5f5cfaad25
ceifa <gabrinelson27@gmail.com>
2019-09-19 13:36:32 -0300

    Improve exception log handlers

---
+++ src/Take.Blip.Builder/OutputProcessingOutput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Take.Blip.Builder.Models;
+
+namespace Take.Blip.Builder
+{
+    /// <summary>
+    /// Represents an exception thrown during the processing of output conditions in the engine.
+    /// </summary>
+    public class OutputProcessingException : BuilderException
+    {
+        /// <inheritdoc />
+        public OutputProcessingException()
+        {
+        }
+
+        /// <inheritdoc />
+        public OutputProcessingException(string message) : base(message)
+        {
+        }
+
+        /// <inheritdoc />
+        public OutputProcessingException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        /// <inheritdoc />
+        protected OutputProcessingException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public string StateId { get; set; }
+
+        public Condition[] OutputConditions { get; set; }
+    }
+}
