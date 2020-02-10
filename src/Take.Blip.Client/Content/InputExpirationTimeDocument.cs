4aa05b6e0b9b31af26985c742c64f34398860b44
TAKENET\pauloh <pauloh@take.net>
2020-02-10 14:45:26 -0300

    Documentation of InputExpirationTimeDocument

--- src/Take.Blip.Client/Content/InputExpirationTimeDocument.cs
+++ src/Take.Blip.Client/Content/InputExpirationTimeDocument.cs
@@ -6,11 +6,17 @@ using System.Text;
 
 namespace Take.Blip.Client.Content
 {
+    /// <summary>
+    /// Represents a expired input with a <see cref="Identity"/> of user to make a fake input
+    /// </summary>
     [DataContract]
     public class InputExpirationTimeDocument : Document
     {
         public const string MIME_TYPE = "application/vnd.blip-client.inputexpirationtime+json";
 
+        /// <summary>
+        /// Identity of user that input was expired. This identity will be used to make a fake input.
+        /// </summary>
         [DataMember(Name = "identity")]
         public Identity Identity { get; set; }
 
