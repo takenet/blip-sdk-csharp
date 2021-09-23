e74e1643e3fd1495c21603d64d2fc6a63c59270a
louisbaggins <luis_araujo.c@icloud.com>
2021-09-23 18:54:35 -0300

    Add: Advanced config extensions

---
+++ src/Take.Blip.Client/Extensions/AdvancedConfig/ConfigurationExtension.cs
@@ -0,0 +1,66 @@
+﻿using Lime.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Take.Blip.Client.Extensions.AdvancedConfig
+{
+    public class ConfigurationExtension : IConfigurationExtension
+    {
+        private readonly ISender _sender;
+
+        public ConfigurationExtension(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        public async Task<Document> GetDomainAsync(string domain, CancellationToken cancellationToken)
+        {
+            if (domain == null) throw new ArgumentNullException(nameof(domain));
+            var requestCommand = new Command()
+            {
+                Method = CommandMethod.Get,
+                Uri = CreateLimeUri(domain)
+            };
+
+            var commandResponse = await _sender.ProcessCommandAsync(requestCommand, cancellationToken);
+            return commandResponse.Resource;
+        }
+
+        public async Task<JsonDocument> GeyKeyValueAsync(string domain, string key, CancellationToken cancellationToken)
+        {
+            if (domain == null) throw new ArgumentNullException(nameof(domain));
+            var configDomain = (await GetDomainAsync(domain, cancellationToken)) as JsonDocument;
+            return configDomain[key] as JsonDocument;
+        }
+
+        public async Task SetConfigAsync(string domain, string key, object value, CancellationToken cancellationToken)
+        {
+            if (domain == null) throw new ArgumentNullException(nameof(domain));
+
+            var resource = new JsonDocument(
+                new Dictionary<string, object>()
+                {
+                    { key, value}
+                },
+                MediaType.ApplicationJson
+                );
+
+            var requestCommand = new Command()
+            {
+                Method = CommandMethod.Set,
+                Uri = CreateLimeUri(domain),
+                Resource = resource
+            };
+
+            await _sender.ProcessCommandAsync(requestCommand, cancellationToken);
+        }
+
+        private string CreateLimeUri(string domain)
+        {
+            return new LimeUri($"lime://{domain}/configuration");
+        }
+    }
+}
---
+++ src/Take.Blip.Client/Extensions/AdvancedConfig/IConfigurationExtension.cs
@@ -0,0 +1,39 @@
+﻿using Lime.Protocol;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Take.Blip.Client.Extensions.AdvancedConfig
+{
+    /// <summary>
+    /// Advanced config extension
+    /// </summary>
+    public interface IConfigurationExtension
+    {
+        /// <summary>
+        /// Get config by domain
+        /// </summary>
+        /// <param name="domain">advanced config domain</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<Document> GetDomainAsync(string domain, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Get config by domain and key
+        /// </summary>
+        /// <param name="domain">advanced config domain</param>
+        /// <param name="key">advanced config key</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<JsonDocument> GeyKeyValueAsync(string domain, string key, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Set config 
+        /// </summary>
+        /// <param name="domain">config domain</param>
+        /// <param name="key">config key</param>
+        /// <param name="value">advanced config value object</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task SetConfigAsync(string domain, string key, object value, CancellationToken cancellationToken);
+    }
+}
