8021601d77edfa19f611432a15fc626bd11a9e3b
João Paulo F. Beltrame <beltramejp@gmail.com>
2022-02-09 13:12:10 -0300

    Remove commentary from tests

--- src/Take.Blip.Builder.UnitTests/AttendanceRedirectHandlerTests.cs
+++ src/Take.Blip.Builder.UnitTests/AttendanceRedirectHandlerTests.cs
@@ -1,7 +1,17 @@
-﻿namespace Take.Blip.Builder.UnitTests
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Lime.Protocol;
+using NSubstitute;
+using Take.Blip.Builder.Hosting;
+using Take.Blip.Client.Extensions.HelpDesk;
+using Takenet.Iris.Messaging.Resources;
+using Xunit;
+
+namespace Take.Blip.Builder.UnitTests
 {
-    public class AttendanceRedirectHandlerFixture //: IDisposable
-    {/*
+    public class AttendanceRedirectHandlerFixture : IDisposable
+    {
         public IContext Context { get; private set; }
         public IAttendanceRedirectHandler AttendanceRedirectHandler { get; private set; }
         public IHelpDeskExtension HelpDeskExtension { get; private set; }
@@ -26,7 +36,7 @@
 
         public void Dispose()
         {
-            
+
         }
     }
 
@@ -43,14 +53,16 @@
         [Fact]
         public async Task RedirectToAttendanceAsync_WhenFeatureToggleIsOff_ShouldNotCallMethods()
         {
-            *//* Arrange *//*
+            /* Arrange */
+            _fixture.Context.ClearReceivedCalls();
+            _fixture.HelpDeskExtension.ClearReceivedCalls();
             MockConfigurationIsAttendanceRedirectAllowed(_fixture.Configuration, false);
 
-            *//* Action *//*
+            /* Action */
             await _fixture.AttendanceRedirectHandler.RedirectToAttendanceAsync(
                 _fixture.FromIdentity, _fixture.UserIdentity, _fixture.Context, _fixture.CancellationToken);
 
-            *//* Assert *//*
+            /* Assert */
             await _fixture.Context.Received(0).GetContextVariableAsync(AttendanceRedirectHandler.ATTENDANCE_REDIRECT_KEY, _fixture.CancellationToken);
             await _fixture.Context.Received(0).SetVariableAsync(AttendanceRedirectHandler.HELP_DESK_HAS_TICKET, true.ToString(), _fixture.CancellationToken);
             await _fixture.HelpDeskExtension.Received(0).CreateTicketAsync(_fixture.FromIdentity, Arg.Any<Ticket>(), _fixture.CancellationToken);
@@ -64,17 +76,17 @@
         [InlineData(null, 0)]
         public async Task RedirectToAttendanceAsync_WithAttendanceRedirect_ShouldMatchNumberOfCallsAsync(string attendanceRedirect, int calls)
         {
-            *//* Arrange *//*
+            /* Arrange */
             _fixture.Context.ClearReceivedCalls();
             _fixture.HelpDeskExtension.ClearReceivedCalls();
             MockContextGetContextVariableAsync(_fixture.Context, AttendanceRedirectHandler.ATTENDANCE_REDIRECT_KEY, attendanceRedirect);
             MockConfigurationIsAttendanceRedirectAllowed(_fixture.Configuration, true);
 
-            *//* Action *//*
+            /* Action */
             await _fixture.AttendanceRedirectHandler.RedirectToAttendanceAsync(
                 _fixture.FromIdentity, _fixture.UserIdentity, _fixture.Context, _fixture.CancellationToken);
 
-            *//* Assert *//*
+            /* Assert */
             await _fixture.Context.Received(calls).SetVariableAsync(AttendanceRedirectHandler.HELP_DESK_HAS_TICKET, true.ToString(), _fixture.CancellationToken);
             await _fixture.HelpDeskExtension.Received(calls).CreateTicketAsync(_fixture.FromIdentity, Arg.Any<Ticket>(), _fixture.CancellationToken);
             await _fixture.Context.Received(calls).DeleteVariableAsync(AttendanceRedirectHandler.ATTENDANCE_REDIRECT_KEY, _fixture.CancellationToken);
@@ -88,6 +100,6 @@
         private void MockContextGetContextVariableAsync(IContext context, string mockString, string mockReturn)
         {
             context.GetContextVariableAsync(mockString, Arg.Any<CancellationToken>()).Returns(mockReturn);
-        }*/
+        }
     }
 }
