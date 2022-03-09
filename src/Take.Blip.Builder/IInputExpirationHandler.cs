014eb920f3066c56d8ff6126e267c4f53b679198
Caio Philipe <caio.philipe@take.net>
2022-03-09 09:51:49 -0300

    Fix error processing user inactive message.
    A recent changed made is passing the context to the SDK when processing
    the input so it can be reused.
    
    When a user inactive message is processed,
    a new input message is created with an empty text and this new message
    should be processed instead.
    
    This changes fixes this by setting the context passed as parameter to
    null when a user inactive message arrives so a new context will be
    created in this case with the new message.

--- src/Take.Blip.Builder/IInputExpirationHandler.cs
+++ src/Take.Blip.Builder/IInputExpirationHandler.cs
@@ -1,15 +1,15 @@
-﻿using System.Threading;
+﻿using Lime.Protocol;
+using System.Threading;
 using System.Threading.Tasks;
-using Lime.Protocol;
 using Take.Blip.Builder.Models;
 
 namespace Take.Blip.Builder
 {
     public interface IInputExpirationHandler
     {
-        Message ValidateMessage(Message message);
+        (bool, Message) ValidateMessage(Message message);
         bool IsValidateState(State state, Message message);
         Task OnFlowPreProcessingAsync(State state, Message message, Node from, CancellationToken cancellationToken);
-        Task OnFlowProcessedAsync(State state, Message message, Node from,  CancellationToken cancellationToken);
+        Task OnFlowProcessedAsync(State state, Message message, Node from, CancellationToken cancellationToken);
     }
 }
