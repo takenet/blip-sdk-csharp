f4d17b56a4134d7da21ccc5e582fa984766f8071
Rafael Santiago <rafael.santiago@take.net>
2022-11-28 08:44:30 -0300

    changed the way of store previous-stateId and stateId context variable to store to a single variable

--- src/Take.Blip.Builder/StateManager.cs
+++ src/Take.Blip.Builder/StateManager.cs
@@ -12,44 +12,60 @@ namespace Take.Blip.Builder
         private const string PREVIOUS_STATE_PREFIX = "previous";
         private const string STATE_ID_KEY = "stateId";
 
-        public Task<string> GetStateIdAsync(IContext context, CancellationToken cancellationToken)
+        private const int STATE_ID_INDEX = 0;
+        private const int PREVIOUS_STATE_ID_INDEX = 1;
+        private const string STATE_AND_PREVIOUS_SEPARATOR = ":";
+
+        public async Task<string> GetStateIdAsync(IContext context, CancellationToken cancellationToken) => ExtractStateId(await GetStateIdContextVariable(context, cancellationToken));
+
+        public async Task<string> GetPreviousStateIdAsync(IContext context, CancellationToken cancellationToken)
         {
-            return context.GetContextVariableAsync(GetStateKey(context.Flow.Id), cancellationToken);
+            var previousStateId = ExtractPreviousStateId(await GetStateIdContextVariable(context, cancellationToken));
+
+            //using GetPreviousStateKey to compatibility with previous versions
+            return previousStateId.IsNullOrEmpty() ? await context.GetContextVariableAsync(GetPreviousStateKey(context.Flow.Id), cancellationToken) : previousStateId;
         }
 
-        public Task<string> GetPreviousStateIdAsync(IContext context, CancellationToken cancellationToken)
-        {
-            return context.GetContextVariableAsync(GetPreviousStateKey(context.Flow.Id), cancellationToken);
-        }
-
-        public Task<string> GetParentStateIdAsync(IContext context, CancellationToken cancellationToken)
+        public async Task<string> GetParentStateIdAsync(IContext context, CancellationToken cancellationToken)
         {
             if (context.Flow.Parent == null)
             {
                 return null;
             }
 
-            return context.GetContextVariableAsync(GetStateKey(context.Flow.Parent.Id), cancellationToken);
+            return ExtractStateId(await GetParentStateIdContextVariable(context, cancellationToken));
         }
 
-        public Task SetStateIdAsync(IContext context, string stateId, CancellationToken cancellationToken)
+        public Task SetStateIdAsync(IContext context, string stateId, string previousStateId, CancellationToken cancellationToken)
         {
             var expiration = context.Flow?.BuilderConfiguration?.StateExpiration ?? default;            
-            return context.SetVariableAsync(GetStateKey(context.Flow.Id), stateId, cancellationToken, expiration);
+            return context.SetVariableAsync(GetStateKey(context.Flow.Id), $"{stateId}{STATE_AND_PREVIOUS_SEPARATOR}{previousStateId}", cancellationToken, expiration);
         }
 
-        public Task SetPreviousStateIdAsync(IContext context, string previousStateId, CancellationToken cancellationToken)
-        {
-            return context.SetVariableAsync(GetPreviousStateKey(context.Flow.Id), previousStateId, cancellationToken);
-        }
-
-        public  Task DeleteStateIdAsync(IContext context, CancellationToken cancellationToken)
-        {
-            return context.DeleteVariableAsync(GetStateKey(context.Flow.Id), cancellationToken);
-        }
+        public  Task DeleteStateIdAsync(IContext context, CancellationToken cancellationToken) => context.DeleteVariableAsync(GetStateKey(context.Flow.Id), cancellationToken);
 
         private static string GetStateKey(string flowId) => $"{STATE_ID_KEY}@{flowId}";
 
         private static string GetPreviousStateKey(string flowId) => $"{PREVIOUS_STATE_PREFIX}-{STATE_ID_KEY}@{flowId}";
+
+        private static string ExtractStateId(string value) => value.IsNullOrEmpty() || !value.Contains(STATE_AND_PREVIOUS_SEPARATOR) ? value : value.Split(':')[STATE_ID_INDEX];
+
+        private static string ExtractPreviousStateId(string value)
+        {
+            if (value.IsNullOrEmpty())
+            {
+                return value;
+            }
+
+            if (value.Contains(STATE_AND_PREVIOUS_SEPARATOR))
+            {
+                return value.Split(STATE_AND_PREVIOUS_SEPARATOR)[PREVIOUS_STATE_ID_INDEX];
+            }
+
+            return "";
+        }
+
+        private Task<string> GetStateIdContextVariable(IContext context, CancellationToken cancellationToken) => context.GetContextVariableAsync(GetStateKey(context.Flow.Id), cancellationToken);
+        private Task<string> GetParentStateIdContextVariable(IContext context, CancellationToken cancellationToken) => context.GetContextVariableAsync(GetStateKey(context.Flow.Parent.Id), cancellationToken);
     }
 }
