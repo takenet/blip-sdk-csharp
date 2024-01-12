dbdb1b10d9c72e549199b4221dbb76fc53d080ff
Túlio Rezende <tulior@blip.ai>
2024-01-12 15:08:46 -0300

    Unit Test creation for sensitive info replacer class

---
+++ src/Take.Blip.Builder.UnitTests/Utils/SensisitveInfoReplacerTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+using Take.Blip.Builder.Utils;
+using Xunit;
+
+namespace Take.Blip.Builder.UnitTests.Utils
+{
+    public class SensisitveInfoReplacerTests : FlowManagerTestsBase
+    {
+        private readonly ISensisitveInfoReplacer _sensisitveInfoReplacer;
+
+        public SensisitveInfoReplacerTests()
+        {
+            _sensisitveInfoReplacer = new SensisitveInfoReplacer();
+        }
+
+        [Fact]
+        public void HeadersSubstitutionShouldSucceed()
+        {
+            //Arrange
+            var contentToSubstitute = @"{""headers"":{""BotKey"":""Key AAAAAAAAAAAAA"",""OtherHeader"":""OtherValue"",""Content-Type"":""application/json""},""method"":""GET"",""uri"":""https://enz557qv71nso.x.pipedream.net""}";
+            var expectedContent = @"{""headers"":{""BotKey"":""***"",""OtherHeader"":""***"",""Content-Type"":""***""},""method"":""GET"",""uri"":""https://enz557qv71nso.x.pipedream.net""}";
+
+            //Act
+            var content = _sensisitveInfoReplacer.ReplaceCredentials(contentToSubstitute);
+
+            //Assert
+            content.ShouldBe(expectedContent);
+        }
+
+        [Fact]
+        public void ContentWithoutHeadersReplaceShouldntChangeAnything()
+        {
+            //Arrange
+            var contentToSubstitute = @"{""headers"":{},""method"":""GET"",""uri"":""https://enz557qv71nso.x.pipedream.net""}";
+            var expectedContent = @"{""headers"":{},""method"":""GET"",""uri"":""https://enz557qv71nso.x.pipedream.net""}";
+
+            //Act
+            var content = _sensisitveInfoReplacer.ReplaceCredentials(contentToSubstitute);
+
+            //Assert
+            content.ShouldBe(expectedContent);
+        }
+    }
+}
