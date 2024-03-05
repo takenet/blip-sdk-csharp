a136863e566f857c83c04dfc389ffde0881b3aa3
Túlio Rezende <tulior@blip.ai>
2024-03-05 16:57:33 -0300

    feat: create actions to block implementation

--- src/Take.Blip.Builder/Variables/SecretVariableProvider.cs
+++ src/Take.Blip.Builder/Variables/SecretVariableProvider.cs
@@ -5,17 +5,26 @@ using System.Threading.Tasks;
 using Lime.Protocol.Serialization;
 using Serilog;
 using Take.Blip.Client;
+using Take.Blip.Builder.Actions.SendMessage;
+using System.Linq;
 
 namespace Take.Blip.Builder.Variables
 {
     public class SecretVariableProvider : ResourceVariableProviderBase, IVariableProvider
     {
+        private readonly string[] _actionsToBlock = { nameof(Actions.SendMessage), nameof(Actions.SetVariable), nameof(Actions.SetBucket) };
+
         public SecretVariableProvider(ISender sender, IDocumentSerializer documentSerializer, ILogger logger) : base(sender, documentSerializer, "secrets", logger) { }
 
         public override VariableSource Source => VariableSource.Secret;
 
         public override async Task<string> GetVariableAsync(string name, IContext context, CancellationToken cancellationToken, string stateActionType = null)
         {
+            if (_actionsToBlock.Contains(stateActionType))
+            {
+                return null;
+            }
+
             var variableValue = await base.GetVariableAsync(name, context, cancellationToken, stateActionType);
 
             return GetSecretValue(variableValue);
