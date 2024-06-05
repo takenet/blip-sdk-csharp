75bd9f839116cbbc71987e492fa4a77683b8263e
Artur Lima Peixoto <artur.peixoto@blip.ai>
2024-06-05 09:31:43 -0300

    included check if error is not Take.blip

--- src/Take.Blip.Builder/FlowManager.cs
+++ src/Take.Blip.Builder/FlowManager.cs
@@ -53,6 +53,8 @@ namespace Take.Blip.Builder
         private const string ACTION_PROCESS_HTTP = "ProcessHttp";
         private const string ACTION_EXECUTE_TEMPLATE = "ExecuteTemplate";
         public const string STATE_ID = "inputExpiration.stateId";
+        private const string START_SOURCE = "take.blip";
+        private const string STATE_TRACE_ERROR_NO_BLIP_NAMESPACE = "Internal Server Error";
 
         public FlowManager(
             IConfiguration configuration,
@@ -315,7 +317,14 @@ namespace Take.Blip.Builder
                             {
                                 if (stateTrace != null)
                                 {
-                                    stateTrace.Error = ex.ToString();
+                                    if (ex.InnerException != null && !ex.InnerException.Source.ToLower().StartsWith(START_SOURCE))
+                                    {
+                                        stateTrace.Error = STATE_TRACE_ERROR_NO_BLIP_NAMESPACE;
+                                    }
+                                    else
+                                    {
+                                        stateTrace.Error = ex.Message;
+                                    }
                                 }
                                 throw;
                             }
