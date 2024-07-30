3366823bda7a52c0954b140cf1ef5a7e8483d328
EmilsonBlip <137826473+EmilsonBlip@users.noreply.github.com>
2024-07-30 18:22:15 -0300

    Organizing comments

--- src/Take.Blip.Client/Extensions/ArtificialIntelligence/ContentAssistant.cs
+++ src/Take.Blip.Client/Extensions/ArtificialIntelligence/ContentAssistant.cs
@@ -5,48 +5,48 @@ using Takenet.Iris.Messaging.Resources.ArtificialIntelligence;
 
 namespace Take.Blip.Client.Extensions.ArtificialIntelligence
 {
-    //
-    // Summary:
-    //     Represents the match of combinations to a result.
+    /// <summary>
+    /// Represents the match of combinations to a result.
+    /// </summary>
     public class ContentAssistant : Document
     {
         public const string MIME_TYPE = "application/vnd.iris.ai.content-assistant+json";
 
         public static readonly MediaType MediaType = MediaType.Parse("application/vnd.iris.ai.content-assistant+json");
 
-        //
-        // Summary:
-        //     Name for easy content identification.
+        /// <summary>
+        /// Name for easy content identification.
+        /// </summary>
         [DataMember(Name = "name")]
         public string Name { get; set; }
 
-        //
-        // Summary:
-        //     Guid content identification.
+        /// <summary>
+        /// Guid content identification.
+        /// </summary>
         [DataMember(Name = "guid")]
         public string Guid { get; set; }
 
-        //
-        // Summary:
-        //     Content created date.
+        /// <summary>
+        /// Content created date.
+        /// </summary>
         [DataMember(Name = "storageDate")]
         public DateTimeOffset StorageDate { get; set; }
 
-        //
-        // Summary:
-        //     The intent/entity combinations to filter for the result.
+        /// <summary>
+        /// The intent/entity combinations to filter for the result.
+        /// </summary>
         [DataMember(Name = "combinations")]
         public ContentCombination[] Combinations { get; set; }
 
-        //
-        // Summary:
-        //     The intent/entity combinations to filter for the result.
+        /// <summary>
+        /// The intent/entity combinations to filter for the result.
+        /// </summary>
         [IgnoreDataMember]
         public string JsonValues { get; set; }
 
-        //
-        // Summary:
-        //     The results of the content.
+        /// <summary>
+        /// The results of the content.
+        /// </summary>
         [DataMember(Name = "results")]
         public Message[] Results { get; set; }
 
