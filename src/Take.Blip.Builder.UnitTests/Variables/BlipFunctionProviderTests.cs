959aaa6043793a6b2d33263566da7cb7ab2ba46d
Artur Peixoto <artur.peixoto@blip.ai>
2024-11-19 17:54:57 -0300

    adapting new variable to be called

---
+++ src/Take.Blip.Builder.UnitTests/Variables/BlipFunctionProviderTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Lime.Messaging.Contents;
+using Lime.Protocol;
+using Lime.Protocol.Serialization;
+using NSubstitute;
+using Serilog;
+using Shouldly;
+using Take.Blip.Builder.Hosting;
+using Take.Blip.Builder.Variables;
+using Take.Blip.Client;
+using Take.Blip.Client.Activation;
+using Xunit;
+
+namespace Take.Blip.Builder.UnitTests.Variables
+{
+    public class BlipFunctionProviderTests : ContextTestsBase
+    {
+        private const string FUNCTION_NAME = "functest";
+        private const string FUCTION_VALUE = "func run() {return 'teste'}";
+
+        public BlipFunctionProviderTests()
+        {
+            Logger = Substitute.For<ILogger>();
+            Sender = Substitute.For<ISender>();
+            DocumentSerializer = new DocumentSerializer(new DocumentTypeResolver());
+
+            Configuration = Substitute.For<IConfiguration>();
+            Application = Substitute.For<Application>();
+            InputContext = new Dictionary<string, object>();
+            Context.InputContext.Returns(InputContext);
+
+            var command = new Command()
+            {
+                Uri = new LimeUri($"/blipFunction/{Uri.EscapeDataString(FUNCTION_NAME)}"),
+                Method = CommandMethod.Get,
+                To = Node.Parse("postmaster@builder.msging.net")
+            };
+
+            var commandResult = new Command()
+            {
+                Status = CommandStatus.Success,
+                Resource = PlainText.Parse(FUCTION_VALUE)
+            };
+
+            Sender.ProcessCommandAsync(Arg.Is<Command>(c => c.Id != null &&
+                                                            c.Method.Equals(command.Method) &&
+                                                            c.To.Equals(command.To) &&
+                                                            c.Uri.Equals(command.Uri)),
+                                      Arg.Any<CancellationToken>())
+                .Returns(commandResult);
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public Application Application { get; }
+
+        public IDictionary<string, object> InputContext { get; }
+
+        public ILogger Logger { get; }
+
+        public ISender Sender { get; }
+
+        public IDocumentSerializer DocumentSerializer { get; }
+
+        public IVariableProvider GetTarget()
+        {
+            return new BlipFunctionVariableProvider(Sender, DocumentSerializer, Logger);
+        }
+
+        [Fact]
+        public async Task GetBlipFunctionValue()
+        {
+            //Arrange
+            var target = GetTarget();
+
+            //Act
+            var actual = await target.GetVariableAsync(FUNCTION_NAME, Context, CancellationToken);
+
+            //Assert
+            actual.ShouldBe(FUCTION_VALUE);
+        }
+    }
+}
---
+++ src/Take.Blip.Builder/Variables/BlipFunctionVariableProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using System.Threading;
+using Lime.Protocol.Serialization;
+using Serilog;
+using Take.Blip.Client;
+
+namespace Take.Blip.Builder.Variables
+{
+    /// <summary>
+    /// Provides variables from Blip functions.
+    /// </summary>
+    public class BlipFunctionVariableProvider : ResourceVariableProviderBase, IVariableProvider
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BlipFunctionVariableProvider"/> class.
+        /// </summary>
+        /// <param name="sender">The sender to use for sending commands.</param>
+        /// <param name="documentSerializer">The document serializer to use.</param>
+        /// <param name="logger">The logger to use for logging.</param>
+        public BlipFunctionVariableProvider(ISender sender, IDocumentSerializer documentSerializer, ILogger logger)
+            : base(sender, documentSerializer, "blipFunction", logger, "postmaster@builder.msging.net") { }
+
+        /// <summary>
+        /// Gets the source of the variable.
+        /// </summary>
+        public override VariableSource Source => VariableSource.BlipFunction;
+
+        /// <summary>
+        /// Gets the value of the specified variable.
+        /// </summary>
+        /// <param name="name">The name of the variable.</param>
+        /// <param name="context">The context in which the variable is being requested.</param>
+        /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+        /// <returns>The value of the variable.</returns>
+        public override async Task<string> GetVariableAsync(string name, IContext context, CancellationToken cancellationToken)
+        {
+            var variableValue = await base.GetVariableAsync(name, context, cancellationToken);
+
+            return variableValue;
+        }
+    }
+}
