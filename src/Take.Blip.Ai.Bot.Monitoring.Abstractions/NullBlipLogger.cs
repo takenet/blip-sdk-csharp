7e0e3564a3f463e26262ac7f295b544e16d3ea27
Artur Peixoto <artur.peixoto@blip.ai>
2025-11-19 14:09:20 -0300

    including Abstractions

---
+++ src/Take.Blip.Ai.Bot.Monitoring.Abstractions/NullBlipLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Take.Blip.Ai.Bot.Monitoring.Abstractions.Models;
+
+namespace Take.Blip.Ai.Bot.Monitoring.Abstractions
+{
+    public sealed class NullBlipLogger : IBlipLogger
+    {
+        public void MessageProcessing(LogInput input) { }
+        public void ActionExecution(LogInput input) { }
+        public void UserContext(LogInput input) { }
+        public void ConversationalFlow(LogInput input) { }
+        public void UserInput(LogInput input) { }
+        public void MessageDelivery(LogInput input) { }
+        public void MissingInfoLatency(LogInput input) { }
+        public void ErrorEvents(LogInput input, Exception exception) { }
+    }
+}
