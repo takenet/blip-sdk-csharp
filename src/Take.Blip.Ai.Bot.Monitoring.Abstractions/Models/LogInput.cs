8b761f4015970c0de1ada1498aea08e37f29adf0
Gustavo Costa Coelho <gustavo.costa@blip.ai>
2026-01-28 14:07:11 -0300

    feat: include new fields

--- src/Take.Blip.Ai.Bot.Monitoring.Abstractions/Models/LogInput.cs
+++ src/Take.Blip.Ai.Bot.Monitoring.Abstractions/Models/LogInput.cs
@@ -5,7 +5,9 @@
         public string Title { get; set; }
         public string IdMessage { get; set; }
         public string From { get; set; }
+        public string OriginalFrom { get; set; }
         public string To { get; set; }
+        public string OriginalTo { get; set; }
         public string? Operation { get; set; }
         public string? EventType { get; set; }
         public object? Data { get; set; }
