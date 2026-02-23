1822e992df034701b2f7a33ad48846c14714ab97
gcostacoelhoBlip <162492962+gcostacoelhoBlip@users.noreply.github.com>
2026-02-23 14:35:34 -0300

    Merge feat/982294-adjust-input-trace-model into master AB#982294
    * feat: include new optional fields and make xml documentation
    
    * fix: reinclude the initializing contructors and remove nullable param
    
    * chore: update newtonsoft package to avoid vulnerability
    
    * Revert "fix: reinclude the initializing contructors and remove nullable param"
    
    This reverts commit 7c33531ff6f3bc6aa4e56569599c62da2d98228d.
    
    * Revert "chore: update newtonsoft package to avoid vulnerability"
    
    This reverts commit b6a4f23eb7c6ce86009c633a7b97fa31dbd96cb5.
    
    * chore: update lime packages with newtonsoft dependencies
    
    * feat: add constructors
    
    * chore: remove nullable permission from proj
    
    * chore: remove unnecessary packages
    
    * chore: remove unecessary package

--- src/Take.Blip.Builder/Diagnostics/InputTrace.cs
+++ src/Take.Blip.Builder/Diagnostics/InputTrace.cs
@@ -1,14 +1,24 @@
-﻿using Lime.Protocol;
+﻿using System;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
+using Lime.Protocol;
 
 namespace Take.Blip.Builder.Diagnostics
 {
+    /// <summary>  
+    /// Represents the input trace for a monitoring event, including owner, flow, user, input, states, and actions.  
+    /// </summary> 
     [DataContract]
     public class InputTrace : Trace
     {
+        /// <summary>  
+        /// The media type for input trace documents.  
+        /// </summary> 
         public new static readonly MediaType MediaType = MediaType.Parse("application/vnd.blip.input-trace+json");
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InputTrace"/> class with the specified media type.
+        /// </summary>
         public InputTrace() : base(MediaType)
         {
             States = new List<StateTrace>();
@@ -17,26 +27,59 @@ namespace Take.Blip.Builder.Diagnostics
             AfterStateChangedActions = new List<ActionTrace>();
         }
 
+        /// <summary>  
+        /// Gets or sets the unique identifier of the input trace.  
+        /// </summary>  
+        [DataMember(Name = "id")]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        /// <summary>  
+        /// Gets or sets the owner identifier associated with this trace.  
+        /// </summary>  
         [DataMember(Name = "owner")]
         public Identity Owner { get; set; }
 
+        /// <summary>  
+        /// Gets or sets the flow identifier associated with this trace.  
+        /// </summary> 
         [DataMember(Name = "flowId")]
         public string FlowId { get; set; }
 
+        /// <summary>  
+        /// Gets or sets the user identifier associated with this trace.  
+        /// </summary>
         [DataMember(Name = "user")]
         public string User { get; set; }
 
+        /// <summary>  
+        /// Gets or sets the input value for this trace.  
+        /// </summary> 
         [DataMember(Name = "input")]
         public string Input { get; set; }
 
+        /// <summary>  
+        /// Gets or sets the collection of state traces associated with this input.  
+        /// </summary> 
         [DataMember(Name = "states")]
         public ICollection<StateTrace> States { get; set; }
 
+        /// <summary>  
+        /// Gets or sets the collection of input actions performed.  
+        /// </summary> 
         [DataMember(Name = "inputActions")]
         public ICollection<ActionTrace> InputActions { get; set; }
 
+        /// <summary>  
+        /// Gets or sets the collection of output actions performed.  
+        /// </summary> 
         [DataMember(Name = "outputActions")]
         public ICollection<ActionTrace> OutputActions { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of event ("AiAgents", "Builder").
+        /// </summary>
+        [DataMember(Name = "eventType")]
+        public string EventType { get; set; } = "Builder";
 
         [DataMember(Name = "afterStateChangedActions")]
         public ICollection<ActionTrace> AfterStateChangedActions { get; set; }
--- src/Take.Blip.Builder/Diagnostics/Trace.cs
+++ src/Take.Blip.Builder/Diagnostics/Trace.cs
@@ -1,9 +1,13 @@
 ﻿using Lime.Protocol;
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.Serialization;
 
 namespace Take.Blip.Builder.Diagnostics
 {
+    /// <summary>
+    /// Base class for monitoring traces.
+    /// </summary>
     [DataContract]
     public class Trace : Document
     {
@@ -18,15 +22,27 @@ namespace Take.Blip.Builder.Diagnostics
         {
         }
 
+        /// <summary>
+        /// TimeStamp of when the trace was created.
+        /// </summary>
         [DataMember(Name = "timestamp")]
         public DateTimeOffset Timestamp { get; set; }
 
+        /// <summary>
+        /// Elapsed time in milliseconds for the operation represented by the trace.
+        /// </summary>
         [DataMember(Name = "elapsedMilliseconds")]
         public long ElapsedMilliseconds { get; set; }
 
+        /// <summary>
+        /// Warning message associated with the trace, if any.
+        /// </summary>
         [DataMember(Name = "warning")]
         public string Warning { get; set; }
 
+        /// <summary>
+        /// Error message associated with the trace, if any.
+        /// </summary>
         [DataMember(Name = "error")]
         public string Error { get; set; }
     }
